bfa24bc6111b34fbfe6efb39252848cad1e0c403
aravndal <aravndal@gmail.com>
2017-10-23 21:06:23 +0200

    Added channel updates + fixes

--- __init__.py
+++ __init__.py
@@ -3,22 +3,26 @@ from thread import start_new_thread
 import logging
 import urllib, json, httplib
 
-DEBUG = True
-thingspeak_api = None 
+DEBUG = False
+thingspeak_api = None
 thingspeak_chnid = None
 thingspeak_ok = None
+thingspeak_api_write = None
+drop_first = None
+url = "api.thingspeak.com"
 
 def log(s):
     if DEBUG:
         cbpi.app.logger.info(s)
 
-def httpPOST(url, path, data_load):
-    log("PUT to URL %s - %s - %s" % (url, path, data_load))
+def httpCon(url, path, data_load, meth='GET'):
+    log("%s to URL %s - %s - %s" % (meth, url, path, data_load))
     try:
         params = urllib.urlencode(data_load)
         conn = httplib.HTTPSConnection(url)
         path += "?" + params
-        conn.request('PUT', path)
+        log("Path: %s" % path)
+        conn.request(meth, path)
         response = conn.getresponse()
         return response
     except Exception as e:
@@ -49,22 +53,28 @@ def thingspeakChnID():
 def thingspeakFields():
     global thingspeak_ok
     global thingspeak_api
+    global thingspeak_api_write
+    global url
     cnt = 1
     if (thingspeak_api == "" or thingspeak_api == ""):
         log("ThingSpeak Config error")
         cbpi.notify("ThingSpeak Error", "Please update config parameter", type="danger")
         return False
+    brewery_name = cbpi.get_config_parameter("brewery_name", "CraftBeerPi")
+    path = "/channels/%s.json" % thingspeak_chnid
+    data_api = "{'api_key':'%s'" % thingspeak_api
+    data = ", 'name':'%s'" % brewery_name
     for key, value in cbpi.cache.get("sensors").iteritems():
-        url = "api.thingspeak.com"
-        path = "/channels/%s.json" % thingspeak_chnid
-        log(url)
-        data = "{'api_key':'%s', 'field%s':'%s'}" % (thingspeak_api, cnt, value.name)
-        log(data)
-        data = eval(data)
-        log(data)
-        result = httpPOST(url, path, data)
-        log("URL Result: %s" % result.read())
+        data += ", 'field%s':'%s'" % (cnt, value.name)
         cnt += 1
+    data += "}"
+    data = data_api + data
+    data = eval(data)
+    result = httpCon(url, path, data, 'PUT')
+    json_result = result.read()
+    json_result = json.loads(json_result)
+    thingspeak_api_write = json_result["api_keys"][0]["api_key"]
+    log("API Write: %s" % thingspeak_api_write)
     thingspeak_ok = True
 
 @cbpi.initalizer(order=9000)
@@ -77,21 +87,32 @@ def init(cbpi):
     if thingspeakChnID is None:
         cbpi.notify("ThingSpeak Error", "Channel ID missing for ThingSpeak plugin, please update and reboot", type="danger")
 
-@cbpi.backgroundtask(key="thingspeak_task", interval=10)
+@cbpi.backgroundtask(key="thingspeak_task", interval=60)
 def thingspeak_background_task(api):
     global thingspeak_ok
+    global url
+    global drop_first
     log("ThingSpeak background task")
     if thingspeak_ok is None:
         thingspeakFields()
     # YOUR CODE GOES HERE
+    if drop_first is None:
+        drop_first = False
+        return False
     if (thingspeak_api == "" or thingspeak_api == ""):
         log("ThingSpeak Config error")
         cbpi.notify("ThingSpeak Error", "Please update config parameter", type="danger")
         return False
+    cnt = 1
+    path = "/update.json"
+    data_api = "{'api_key':'%s'" % thingspeak_api_write
+    data = ""
     for key, value in cbpi.cache.get("sensors").iteritems():
-        log(key)
-        log(value)
-        log(value.name)
-        log(value.instance.get_unit())
-        log(value.instance.last_value)
+        data += ", 'field%s':'%s'" % (cnt, value.instance.last_value)
+        cnt += 1
+    data += "}"
+    data = data_api + data
+    data = eval(data)
+    result = httpCon(url, path, data)
+    log("URL Result: %s" % result.read())
 
